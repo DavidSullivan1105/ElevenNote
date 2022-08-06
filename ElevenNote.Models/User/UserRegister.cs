57c21664e82dee02f02ff04b4ed4c45c630e5295
David Sullivan <dcsulliv@gmail.com>
2022-08-06 16:36:41 -0400

    implementing passwordhasher

--- ElevenNote.Models/User/UserRegister.cs
+++ ElevenNote.Models/User/UserRegister.cs
@@ -1,24 +1,24 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
-namespace ElevenNote.Data.Entities
+using System.ComponentModel.DataAnnotations;
+namespace ElevenNote.Models.User
 {
-    public class UserEntity
+    public class UserRegister
     {
-        [Key]
-        public int Id { get; set; }
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
+        [MinLength(4)]
         public string UserName { get; set; }
         [Required]
+        [MinLength(4)]
         public string Password { get; set; }
-        public string FirstName { get; set; }
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        public string FirstName {get; set;}
         public string LastName { get; set; }
-        [Required]
-        public DateTime DateCreated { get; set; }
     }
 }
--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -2,11 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Models.User;
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // public Task<bool> RegisterUserAsync(UserRegister model)
+        // {
+        //     throw new NotImplementedException();
+        // }
+
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            if(await GetUserByEmailAsync(model.Email) != null || await GetUserByUsernameAsync(model.UserName) != null)
+            return false;
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+                Password = model.Password,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateCreated = DateTime.Now
+            };
+
+            _context.Users.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+            
+            return numberOfChanges == 1;
+        }
+
+        private async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+        }
+
+        private async Task<UserEntity> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.UserName.ToLower() == username.ToLower());
+        }
+
     }
 }
